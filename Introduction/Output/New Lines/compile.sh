63b4c242d71f5a40f414d870f7691c01572ab226
Sean Osborne <sean@sargames.xyz>
2025-07-15 18:47:35 +0100

    Learning up to 15/07/25 at 18:47.

---
+++ Introduction/Output/New Lines/compile.sh
@@ -0,0 +1,5 @@
+# Quick warning, this will only work if you are in the directory of the C script, otherwise this script will fail.
+
+gcc newlines.c -o newlines.o
+chmod +x newlines.o
+./newlines.o
---
+++ Introduction/Output/New Lines/newlines.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("This has newlines.\n");
+    printf("Meaning this text is on a new line. (Self explanatory.)\n");
+    printf("Yipee!\n\n");
+
+    printf("You can do two newlines to seperate text, and you can also use a newline inside of a pre-existing printf, instead of creating a new one.\nAs shown here.\n");
+    return 0;
+}
